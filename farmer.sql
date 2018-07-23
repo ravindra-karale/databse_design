7bb5e592898af81073507fed0486d8f130c6557a
ravindra-karale <ravindra.karale@wgblindia.com>
2018-07-23 13:36:28 +0530

    one queury remaining.

--- farmer.sql
+++ farmer.sql
@@ -1,14 +1,14 @@
-drop TABLE Notifications;
-drop TABLE Farmer_Crops;
-drop TABLE Media;
-drop TABLE Complaints;
-drop TABLE Advertise;
-drop TABLE Product_Sale_Details;
-drop TABLE Product_Sale;
-drop TABLE Product_Ratting;
-drop TABLE Product_Bidding;
-drop TABLE Product;
-drop TABLE Users;
+drop TABLE notifications;
+drop TABLE farmer_crops;
+drop TABLE media;
+drop TABLE complaints;
+drop TABLE advertise;
+drop TABLE product_sale_details;
+drop TABLE product_sale;
+drop TABLE product_rating;
+drop TABLE product_bidding;
+drop TABLE product;
+drop TABLE users;
 drop TABLE role;
 
 CREATE TABLE role(
@@ -16,7 +16,7 @@ CREATE TABLE role(
   name VARCHAR (20) UNIQUE NOT NULL
 );
 
-CREATE TABLE Users (
+CREATE TABLE users (
   id serial PRIMARY KEY,
   role_id INTEGER REFERENCES role(id) ON DELETE CASCADE,
   username VARCHAR (50) UNIQUE NOT NULL,
@@ -33,21 +33,20 @@ CREATE TABLE Users (
 
 CREATE TYPE status AS ENUM ('0', '1', '2');
 
-CREATE TABLE Product(
+CREATE TABLE product(
   id serial PRIMARY KEY,
-  user_id INTEGER REFERENCES Users (id) ON DELETE CASCADE,
-  name VARCHAR (50) UNIQUE NOT NULL,
+  user_id INTEGER REFERENCES users (id) ON DELETE CASCADE,
+  name VARCHAR (50) NOT NULL,
   description VARCHAR (100) NOT NULL,
   product_status status NOT NULL,
   purchase_price REAL NOT NULL,
   sale_price REAL NOT NULL,
   created_on TIMESTAMP NOT NULL
-  -- PRIMARY KEY (id, user_id)
-  -- FOREIGN KEY (user_id) REFERENCES Users (id)
 );
 
-CREATE TABLE Notifications(
+CREATE TABLE notifications(
   id serial PRIMARY KEY,
+  product_id INTEGER REFERENCES product (id) ON DELETE CASCADE,
   title VARCHAR(50) NOT NULL,
   url VARCHAR(100) UNIQUE,
   description VARCHAR(100) NOT NULL,
@@ -55,9 +54,9 @@ CREATE TABLE Notifications(
 );
 
 
-CREATE TABLE Advertise(
+CREATE TABLE advertise(
   id serial,
-  user_id INTEGER REFERENCES Users (id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users (id) ON DELETE CASCADE,
   title VARCHAR (50) NOT NULL,
   description VARCHAR (100) NOT NULL,
   created_on TIMESTAMP NOT NULL,
@@ -65,12 +64,12 @@ CREATE TABLE Advertise(
   PRIMARY KEY (id, user_id)
 );
 
-CREATE TABLE Product_Bidding(
+CREATE TABLE product_bidding(
   id serial,
-  user_id INTEGER REFERENCES Users (id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users (id) ON DELETE CASCADE,
   product_name VARCHAR(50) NOT NULL,
   description VARCHAR(100) NOT NULL,
-  quntity REAL NOT NULL,
+  quantity REAL NOT NULL,
   measure_unit VARCHAR(50),
   cureency VARCHAR(20),
   baseprice REAL NOT NULL,
@@ -79,62 +78,56 @@ CREATE TABLE Product_Bidding(
   PRIMARY KEY (id, user_id)
 );
 
-CREATE TABLE Complaints(
+CREATE TABLE complaints(
   id serial,
-  user_id INTEGER REFERENCES Users (id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users (id) ON DELETE CASCADE,
   description VARCHAR(100),
   PRIMARY KEY (id, user_id)
 );
 
-CREATE TABLE Product_Sale(
+CREATE TABLE product_sale(
   id serial PRIMARY KEY,
   order_id INTEGER UNIQUE,
-  product_id INTEGER REFERENCES Product (id) ON DELETE CASCADE,
-  -- user_id INTEGER REFERENCES Users (id) ON DELETE CASCADE,
+  product_id INTEGER REFERENCES product (id) ON DELETE CASCADE,
   created_on TIMESTAMP NOT NULL
-  -- PRIMARY KEY (id, order_id)
 );
 
-CREATE TABLE Product_Ratting(
+CREATE TABLE product_rating(
   id serial,
-  product_id INTEGER REFERENCES Product (id) ON DELETE CASCADE,
-  user_id INTEGER REFERENCES Users (id) ON DELETE CASCADE,
-  ratting REAL NOT NULL,
+  product_id INTEGER REFERENCES product (id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users (id) ON DELETE CASCADE,
+  rating REAL NOT NULL,
   created_on TIMESTAMP NOT NULL,
   PRIMARY KEY (id, product_id, user_id)
 );
 
 
 
-CREATE TABLE Product_Sale_Details(
+CREATE TABLE product_sale_details(
   id serial PRIMARY KEY,
-  product_sale_id int REFERENCES Product_Sale (id) ON DELETE CASCADE,
-  -- product_id int REFERENCES Product_Sale (id) ON DELETE CASCADE,
+  product_sale_id int REFERENCES product_sale (id) ON DELETE CASCADE,
   purchase_price REAL NOT NULL,
   sale_price REAL not NULL,
-  quntity INTEGER NOT NULL
-  -- PRIMARY KEY (id, order_id, product_id)
+  quantity INTEGER NOT NULL
 );
 
 
 
-CREATE TABLE Media(
+CREATE TABLE media(
   id serial PRIMARY KEY,
-  product_id INTEGER REFERENCES Product (id) ON DELETE CASCADE,
+  product_id INTEGER REFERENCES product (id) ON DELETE CASCADE,
   image VARCHAR(100),
   video VARCHAR(100)
-  -- PRIMARY KEY (id, product_id)
 );
 
-CREATE TABLE Farmer_Crops(
+CREATE TABLE farmer_crops(
   id serial PRIMARY KEY,
-  user_id INTEGER REFERENCES Users (id) ON DELETE CASCADE,
-  media_id INTEGER REFERENCES Media (id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users (id) ON DELETE CASCADE,
+  media_id INTEGER REFERENCES media (id) ON DELETE CASCADE,
   crop_name VARCHAR(50) NOT NULL,
   description VARCHAR(100) NOT NULL,
-  quntity REAL NOT NULL,
+  quantity REAL NOT NULL,
   duration VARCHAR(50)
-  -- PRIMARY KEY (id, user_id)
 );
 
 -- Insert into role values
@@ -143,34 +136,55 @@ INSERT INTO role VALUES(DEFAULT,'retailers');
 INSERT INTO role VALUES(DEFAULT,'admin');
 
 -- insert into User values
-INSERT INTO Users VALUES(DEFAULT,1,'ravi','ravi','abc@gmail.com','pune sfsf,sad','ahmednagar','maharashtra','india','2017-06-22 19:10:25');
-INSERT INTO Users VALUES(DEFAULT,1,'ravi1','ravi1','abc1@gmail.com','pune sfsf,sad','ahmednagar','maharashtra','india','2018-06-22 19:10:25');
-INSERT INTO Users VALUES(DEFAULT,1,'ravi2','ravi12','abc2@gmail.com','pune sfsf,sad','ahmednagar','maharashtra','india','2018-06-22 19:10:25');
-INSERT INTO Users VALUES(DEFAULT,1,'ravi3','ravi123','abc3@gmail.com','pune sfsf,sad','ahmednagar','maharashtra','india','2018-06-22 19:10:25');
-INSERT INTO Users VALUES(DEFAULT,1,'ravi4','ravi1234','abc4@gmail.com','pune sfsf,sad','ahmednagar','maharashtra','india','2018-06-22 19:10:25');
-INSERT INTO Users VALUES(DEFAULT,2,'rvi','rvi','rvi@gmail.com','pune sfsf,sad','ahmednagar','maharashtra','india','2018-07-01 19:10:25');
+INSERT INTO users VALUES(DEFAULT,1,'ravi','ravi','abc@gmail.com','pune sfsf,sad','ahmednagar','maharashtra','india','2017-06-22 19:10:25');
+INSERT INTO users VALUES(DEFAULT,1,'ravi1','ravi1','abc1@gmail.com','pune sfsf,sad','ahmednagar','maharashtra','india','2018-06-22 19:10:25');
+INSERT INTO users VALUES(DEFAULT,1,'ravi2','ravi12','abc2@gmail.com','pune sfsf,sad','ahmednagar','maharashtra','india','2018-06-22 19:10:25');
+INSERT INTO users VALUES(DEFAULT,1,'ravi3','ravi123','abc3@gmail.com','pune sfsf,sad','ahmednagar','maharashtra','india','2018-06-22 19:10:25');
+INSERT INTO users VALUES(DEFAULT,1,'ravi4','ravi1234','abc4@gmail.com','pune sfsf,sad','ahmednagar','maharashtra','india','2018-06-22 19:10:25');
+INSERT INTO users VALUES(DEFAULT,2,'rvi','rvi','rvi@gmail.com','pune sfsf,sad','ahmednagar','maharashtra','india','2018-07-01 19:10:25');
+INSERT INTO users VALUES(DEFAULT,2,'rdsvi1','rvdsi1','rvids1@gmail.com','pune dfefwccxsfsf,sad','pune','maharashtra','india','2018-02-03 19:10:25');
+INSERT INTO users VALUES(DEFAULT,2,'rcxxvi','rvxci','rvi@gmxcail.com','pune sfsf,sad','ahmednagar','maharashtra','india','2018-07-01 19:10:25');
+INSERT INTO users VALUES(DEFAULT,2,'raavi1','rvaai1','rvi1@gmaaail.com','pune dfefwccxsfsf,sad','pune','maharashtra','india','2018-02-03 19:10:25');
+
 
 -- insert into Product values
-INSERT INTO Product VALUES(DEFAULT,2,'Harit Biyane','dfdfdfdfdfdfdfdfdfd','1','105.50','110','2018-07-01 19:10:25');
-INSERT INTO Product VALUES(DEFAULT,2,'Uttam Beej','dfdfdfdfdfdfdfdfdfd','1','500','600','2018-07-01 19:10:25');
-INSERT INTO Product VALUES(DEFAULT,2,'Janukiy biyane','dfdfdfdfdfdfdfdfdfd','1','305.50','410','2018-07-01 19:10:25');
-INSERT INTO Product VALUES(DEFAULT,2,'Biyane','dfdfdfdfdfdfdfdfdfd','1','705.50','810','2018-07-01 19:10:25');
-
+INSERT INTO product VALUES(DEFAULT,6,'Harit Biyane','dfdfdfdfdfdfdfdfdfd','1','105.50','110','2018-07-01 19:10:25');
+INSERT INTO product VALUES(DEFAULT,7,'Uttam Beej','dfdfdfdfdfdfdfdfdfd','1','500','600','2018-07-01 19:10:25');
+INSERT INTO product VALUES(DEFAULT,8,'Janukiy biyane','dfdfdfdfdfdfdfdfdfd','1','305.50','410','2018-07-01 19:10:25');
+INSERT INTO product VALUES(DEFAULT,9,'cotton 5151 Biyane','dfdfdfdfdfdfdfdfdfd','1','705.50','810','2018-07-01 19:10:25');
 
   -- insert into product sale values
-INSERT INTO Product_Sale values(DEFAULT,101,1,'2018-04-01 1:10:25');
-INSERT INTO Product_Sale values(DEFAULT,102,2,'2018-01-01 1:10:25');
-INSERT INTO Product_Sale values(DEFAULT,103,3,'2018-05-01 1:10:25');
+INSERT INTO product_sale values(DEFAULT,101,1,'2018-04-01 1:10:25');
+INSERT INTO product_sale values(DEFAULT,102,2,'2018-01-01 1:10:25');
+INSERT INTO product_sale values(DEFAULT,103,3,'2018-05-01 1:10:25');
+INSERT INTO product_sale values(DEFAULT,104,1,'2018-05-01 1:10:25');
+INSERT INTO product_sale values(DEFAULT,105,2,'2018-05-01 1:10:25');
+INSERT INTO product_sale values(DEFAULT,106,4,'2018-05-01 1:10:25');
 
 
   -- insert into product sale details values
-INSERT INTO Product_Sale_Details values(DEFAULT,1,105.50,110,50);
-INSERT INTO Product_Sale_Details values(DEFAULT,2,500,600,150);
-INSERT INTO Product_Sale_Details values(DEFAULT,3,705.50,810,500);
+INSERT INTO product_sale_details values(DEFAULT,1,105.50,110,500);
+INSERT INTO product_sale_details values(DEFAULT,2,500,600,150);
+INSERT INTO product_sale_details values(DEFAULT,3,305.50,410,5);
+INSERT INTO product_sale_details values(DEFAULT,1,105.50,110,400);
+INSERT INTO product_sale_details values(DEFAULT,2,500,600,350);
+INSERT INTO product_sale_details values(DEFAULT,4,705.50,810,50);
 
--- isert value into product ratting
 
-INSERT INTO Product_Ratting values(DEFAULT,1,2,4.5,'2018-05-01 1:10:25');
-INSERT INTO Product_Ratting values(DEFAULT,2,2,4.0,'2018-06-01 1:10:25');
-INSERT INTO Product_Ratting values(DEFAULT,3,2,5.0,'2018-08-01 1:10:25');
 
+-- isert value into product rating
+
+INSERT INTO product_rating values(DEFAULT,1,2,4.5,'2018-05-01 1:10:25');
+INSERT INTO product_rating values(DEFAULT,2,2,4.0,'2018-06-01 1:10:25');
+INSERT INTO product_rating values(DEFAULT,3,2,5.0,'2018-08-01 1:10:25');
+INSERT INTO product_rating values(DEFAULT,1,2,5.0,'2018-08-01 1:10:25');
+INSERT INTO product_rating values(DEFAULT,2,2,4.2,'2018-08-01 1:10:25');
+INSERT INTO product_rating values(DEFAULT,4,2,4.5,'2018-08-01 1:10:25');
+
+select DISTINCT t1.id,t1.name,t3.TotalQuantity from product t1 inner join product_sale t2 on t1.id=t2.product_id inner join (SELECT product_sale_id, SUM(quantity) AS TotalQuantity FROM product_sale_details GROUP BY product_sale_id ORDER BY SUM(quantity) DESC limit 1) t3 on t2.product_id=t3.product_sale_id;
+
+select t1.id,t1.name,avg as rating,TotalQuantity from product t1 inner join (select product_id,avg(rating) from product_rating group by product_id order by avg(rating) desc) t2 on t1.id=t2.product_id inner join (SELECT  product_sale_id, SUM(quantity) AS TotalQuantity FROM product_sale_details GROUP BY product_sale_id ORDER BY SUM(quantity) DESC ) t3 on t2.product_id=t3.product_sale_id;
+
+SELECT order_id,product_id FROM product_sale WHERE product_id='1' and EXTRACT(MONTH FROM created_on) >= 1;
+
+select product_id,to_char(created_on,'mon') as Moth from product_sale where product_id in(select id from product where user_id=6);
